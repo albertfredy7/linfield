a4c6d262546e7fe88a7f146327180adee73def08
albertfredy7 <albertfredy007@gmail.com>
2024-03-07 16:10:28 +0530

    sidebar finished but need to fix the logout button for the tablet screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Insights from './Pages/Insights';
 import Home from './Pages/Home';
 import Login from './Pages/Login'; // Make sure to import the Login component
+import ExpenseTracker from './Pages/ExpenseTracker';
+import AddStudent from './Pages/AddStudent';
+import ModifyStudent from './Pages/ModifyStudent';
+import FilterStudents from './Pages/FilterStudents';
+import CreateTeacher from './Pages/CreateTeacher';
+import UpdateFee from './Pages/UpdateFee';
 
 function App() {
  const location = useLocation();
@@ -13,6 +19,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/insights" element={<Insights />} />
+        <Route path="/expense" element={<ExpenseTracker />} />
+        <Route path="/add" element={<AddStudent />} />
+        <Route path="/modify" element={<ModifyStudent />} />
+        <Route path="/filter" element={<FilterStudents />} />
+        <Route path="/teacherId" element={<CreateTeacher />} />
+        <Route path="/updatefee" element={<UpdateFee />} />
+
+
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
--- src/Pages/UpdateFee.jsx
+++ src/Pages/UpdateFee.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Sidebar from '../Components/Sidebar'
 
-function Home() {
+function UpdateFee() {
   return (
     <div className='bg-[#f0f0f0]'>
         <div className='grid grid-cols-5'>
             <div className='w-full '>
                 <Sidebar />
             </div>
-            <div>Home</div>
+            <div>Fee</div>
         </div>
         
     </div>
   )
 }
 
-export default Home
+export default UpdateFee
