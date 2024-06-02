7ac9d067e7a7751d94247124693fdc35cbe7fb71
abie_x <abhiramzmenon@gmail.com>
2024-06-02 19:34:23 +0530

    idk

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { teacherLoginReducer } from './reducers/teacherReducers';
 import { createStore, combineReducers } from 'redux';
